816a316b72a5ca385458a1b432f9140319140beb
saanu512 <154816187+saanu512@users.noreply.github.com>
2026-09-16 21:51:50 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cwp-v40-gemini-ai-firebase-logic-fix5';
+const CACHE='cwp-v40-gemini-firebase-ai-logic-final1';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./firebase.js','./manifest.json']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
