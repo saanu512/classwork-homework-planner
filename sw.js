e1736bec30cf57c5988c1156815187115e9fd2d3
saanu512 <154816187+saanu512@users.noreply.github.com>
2026-09-17 23:19:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,5 +10,3 @@ self.addEventListener('fetch',e=>{
     e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match('./index.html'))));
   }
 });
-
-// navigation readability update
