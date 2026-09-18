03fc74912f46be3330f5e0225cc8d40c2786e8d0
saanu512 <154816187+saanu512@users.noreply.github.com>
2026-09-18 07:46:16 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cwp-v40-dark-purple-blue-v43-syllabus-light-fix';
+const CACHE='cwp-v40-nav-readable-auth-profile-v44';
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./firebase.js','./manifest.json']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{
